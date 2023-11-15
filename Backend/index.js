9c647e53d0b7e124472fef50e9335e87319d7078
David Martz <d86.martz@gmail.com>
2023-11-15 17:19:35 +0100

    Filters eventListener

--- Backend/index.js
+++ Backend/index.js
@@ -8,30 +8,36 @@ const getWorksList = async () => {
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        document.addEventListener('filters', () => {
-            let allButton = document.createElement('button');
-            button.type = 'button';
-            button.innerHTML = 'Tous';
-            button.onclick = function () {
 
-            };
-            let sectionFilter = document.querySelector(".gallery");
-            sectionFilter.appendChild(allButton);
-        })
+        data = []
+
+        const buttons = document.querySelectorAll(".filters")
+            for(let i = 0; i < buttons.length; i++) {
+                buttons[i].addEventListener('click', (e) => {
+                    console.log("Filter selected :", e.target.id)
+                })
+            }
         
-        for(let i = 0; i < data.length; i++) {
+        const filterWorks = () => {
             let work = document.createElement('work');
             let image = document.createElement('img');
             let title = document.createElement('p');
             title.style.marginTop = "0.5em";
             image.src = `${data[i].imageUrl}`;
             title.textContent = `${data[i].title}`;
-            let sectionGallery = document.querySelector(".gallery");
+            sectionGallery = document.querySelector(".gallery");
             work.appendChild(image);
             work.appendChild(title);
-            sectionGallery.appendChild(work);
+            sectionGallery.appendChild(work)
         }
-    })        
-}  
-  
+        
+        for(let i = 0; i < data.length; i++) {
+            filterWorks ()
+        }
+    })
+}        
+
 getWorksList()
+
+
+        
