1294d7b9fa049e8184583c83b2e69a722a4842a2
David Martz <d86.martz@gmail.com>
2023-11-17 15:51:46 +0100

    Filters list

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,50 +1,77 @@
 //Récupération des projets sur l'API
-async function apiData () {
-     await fetch ("http://localhost:5678/api/works", {
-        method: "GET",
-        headers: {
-        Accept: "application/json",
-        }
-    })
+const apiData = async () => {
+  await fetch ("http://localhost:5678/api/works", {
+    method: "GET",
+    headers: {
+    Accept: "application/json",
+    }
+  })
 
-    .then((res) => res.json())
-    .then((data) => {
-        worksList = data;
-    })
- 
-    //Création des catégories
-    
-setWorks = new Set(worksList);
-const setIterator = setWorks.values()
-for (let i = 0; i < worksList.length; i++) {
-  let name = setIterator.next().value.category.name;
-  let btnCat = document.createElement("button");
-  btnCat.textContent = name;
-  document.querySelector(".filters").appendChild(btnCat)  
-}
-
-const buttons = document.querySelectorAll(".filters");
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", (e) => {
-    console.log("Filter selected :", e.target.id);
+  .then((res) => res.json())
+  .then((data) => {
+    worksList = data;
+    console.log("List of project :",data)
   });
-}
 
-const filterWorks = (i) => {
-  let work = document.createElement("work");
+    //Création des filtres
+
+//Création du Set des projets
+setWorksList = new Set(worksList);
+
+//Renvoie des valeurs du Set
+const worksListIter = setWorksList.values();
+
+//Récupération des noms de filtre à partir des valeurs récupérées
+const catWorks = new Set();
+catWorks.add("Tous")
+for (let i = 0; i < worksList.length; i++) {
+  let catName = worksListIter.next().value.category.name;
+  catWorks.add(catName)
+};
+console.log("Category of project :", catWorks)
+const catWorksIter = catWorks.values();
+
+//Création des boutons de filtre des projets
+for (let i = 0; i < catWorks.size; i++) {
+  let btnCat = document.createElement("button");
+  btnCat.textContent = catWorksIter.next().value;
+  document.querySelector(".filters").appendChild(btnCat)
+};
+
+    //Création des listes de projet par filtres
+
+//Création de la base des figures de projet
+const creaWork = (i) => {
+  let figure = document.createElement("work");
   let image = document.createElement("img");
   let title = document.createElement("p");
   title.style.marginTop = "0.5em";
   image.src = worksList[i].imageUrl;
   title.textContent = worksList[i].title;
   sectionGallery = document.querySelector(".gallery");
-  work.appendChild(image);
-  work.appendChild(title);
-  sectionGallery.appendChild(work);
+  figure.appendChild(image);
+  figure.appendChild(title);
+  sectionGallery.appendChild(figure);
 };
 
-for (let i = 0; i < worksList.length; i++) {
-  filterWorks(i);
+//Incrémentation des listes par filtres
+
+const buttons = document.querySelectorAll(".filters");
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", (e) => {
+    console.log("Filter selected :", e.target.textContent);
+    for (let i = 0; i < worksList.length; i++) {
+      if (catName===e.target.textContent){ 
+        creaWork(i)
+      }
+      else { 
+        for (let i = 0; i < worksList.length; i++) {
+        creaWork(i)
+        }
+      }
+    }
+  })
 }
+
 }
 apiData()
