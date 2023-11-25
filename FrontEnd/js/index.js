d9f7b7f68d2b09203e350a5581d990c738016c6b
David Martz <d86.martz@gmail.com>
2023-11-25 16:04:52 +0100

    filtre et galerie ok
    a voir: focus

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,71 +1,76 @@
-    //Récupération des données de l'API.
-const worksData = [];
-const worksCategory = new Set()
-const apiData = async () => {
-  await fetch ("http://localhost:5678/api/works/")
-  .then((res) => res.json())
-  .then((data) => { console.log(data);
-    data.forEach(i => {
-    worksData.push(i)
-    worksCategory.add(i.category.name)
+const worksData = [];             //Creating a table for receiving projects
+const worksCategory = new Set()   //Creating a Set for Receiving Project Categories Without Duplicates
+
+  //Retrieving API data
+
+const getApiData = async () => { 
+  await fetch ("http://localhost:5678/api/works/")                //Calling the API for data receipt
+  .then((res) => res.json())                                      //Converting data to JSON format for processing
+  .then((data) => { console.log("Projects on dataBase", data);    //Data Report
+    data.forEach(element => {
+    worksData.push(element);                                      //Data storage 
+    worksCategory.add(element.category.name)                      //Data storage
   })})
+  console.log("Categories of Project", worksCategory)
 
-
-    //Création des boutons filtre
-
-const sectionFilters = document.querySelector(".filters");  
-const filterButton = (name) => {
-  button = document.createElement("button");
-  button.textContent = (name);
-  sectionFilters.appendChild(button)
+    //Creating Filter Buttons
+  
+const sectionFilters = document.querySelector(".filters");  //Path to HTML section 
+const filterButton = (category) => {  
+  button = document.createElement("button");                //Creating the Button
+  button.setAttribute("id", category);                      //Assigning an Id
+  button.textContent = (category);                          //Visible button text
+  sectionFilters.appendChild(button)                        //Added button in HTML section
 }
-filterButton("Tous");
-worksCategory.forEach(i => {
-  filterButton(i)
-})
+filterButton("Tous")
+worksCategory.forEach(category => {                         //Creating Buttons for Category List Items
+  filterButton(category)
+});
 
-    //Création de la figure.
+    //Creating Project Figures
 
-sectionGallery = document.querySelector(".gallery");
-const figureCreation = () => {
-  figure = document.createElement("work");
-  image = document.createElement("img");
-  title = document.createElement("p");
-  title.style.marginTop = "0.5em";
-  image.src = worksData[i].imageUrl;
-  title.textContent = worksData[i].title;                                 
-  figure.appendChild(image);
-  figure.appendChild(title);
-  sectionGallery.appendChild(figure)
-}
+  sectionGallery = document.querySelector(".gallery");    //Path to HTML section
+  const figureCreation = (element) => {
+    figure = document.createElement("work");              //Creating the Project Figure Element
+    image = document.createElement("img");                //Creating the Project Image Element
+    title = document.createElement("p");                  //Creating the Project Title Element
+    title.style.marginTop = "0.5em";                      //Changing the Layout of the Title
+    image.src = element.imageUrl;                         //Image Element Source
+    title.textContent = element.title;                    //Title Element Source
+    figure.appendChild(image);                            //Added the Image Element in the Figure Element
+    figure.appendChild(title);                            //Added the Title Element in the Figure Element
+    sectionGallery.appendChild(figure)                    //Added the element in the HTML section
+  };
 
-    //Fonction pour Création des figures de tous les projets à la fois.
-const figureForAll = () => {
-  sectionGallery.remove();
-  worksData.forEach((i) => {
-    figureCreation(i)
-  })
-}
-    //Préchargement de toutes les figures au chargement de la page.
-window.onload = () => {
-  figureForAll()    
-}
+    //Viewing Figures
 
-    //Chargement des figures selon le filtre.
-sectionFilters.addEventListener("click", (e) => {
-  let selection = e.target.textContent
-  console.log(selection)
-  if (worksCategory.has(selection)) {
-    sectionGallery.remove();
-    for (i = 0; i < worksData.length; i++) {
-      if (i.category.name.value === selection)
-        figureCreation(i)
+sectionFilters.addEventListener("click", (event) => {             //Listen to filter selection buttons
+  sectionGallery.innerHTML = "";                                  //Deletion of the present selection                 
+  if (worksCategory.has(event.target.textContent)) {              //Comparison of the selection with the list of categories
+    worksData.forEach(element => {
+      if(element.category.name === event.target.textContent) {    //Creation of figures based on selection
+        figureCreation(element)
       }
-    }  
-  else { 
-    figureForAll()
+    })
+  }    
+  else {
+    sectionGallery.innerHTML = ""; 
+    worksData.forEach((element) => {    //Creation of figures if the selection is "All"
+      figureCreation(element)           
+    })
   }
 })
 
+//Page load selection
+
+window.onload = () => {                                     
+  let filterActived = document.getElementById("Tous");    //Selecting the "All" button
+  filterActived.click();                                  //Activating the button
+  filterActived.focus();
+  sectionGallery.innerHTML = "";                                  //Activating the Pseudo-Class Focus of the Button
+  worksData.forEach((element) => {                        //Creating Figures
+    figureCreation(element)                               
+  })
+};   
 }
-apiData()
+getApiData()    //Launching the Data Recovery Feature 
