95c9200c6ff0e0c854a768fcc93d9f66b9ea4996
David Martz <d86.martz@gmail.com>
2023-12-04 18:50:37 +0100

    authentification

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,7 +1,5 @@
 const apiUrl = "http://localhost:5678/api/"
 
-
-
 const login = () => {
     loginBox = document.getElementById("login")
     loginBox.addEventListener("submit", (event) => {
@@ -16,32 +14,23 @@ const authentification = async () => {
     const password = document.getElementById("pass").value;
     console.log(email);
     console.log(password);
-    return await fetch (`${apiUrl}users/login`), {
+    return await fetch (`${apiUrl}users/login`, {
         method: 'POST',
         body: JSON.stringify ({        
             "email": email,
             "password": password    
         }),
         headers: {
-        "accept": "application/json",
         "Content-Type": "application/json"
         }
+    })
     .then(res => res.json())
-    .then(res => console.log(res))
-    }
-    
+    .then((data) => {
+        if (data.error) {
+            alert("Error Password or E-mail");
+        } else {
+            sessionStorage.setItem("token", (data.token.JSON));
+            window.location.assign("./index.html");
+        }
+      })
 }
- 
-//     .then(data => {
-//         console.log(data)
-//         if (data.error) {
-//             alert("Error Password or E-mail");
-//         } else {
-//             window.location.href = './index.html';
-//             sessionStorage.setItem("token", (data.token.JSON))
-//         }
-//     })}
-       
-// }
-
-
